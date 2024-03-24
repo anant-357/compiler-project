977ede6e93a0164a06e18ec1b6d3998e485b3fad
anant-357 <anantsh.88@gmail.com>
2024-03-24 16:29:13 +0530

    fixed custom-module-pass

--- pdg/custom-module-pass.cpp
+++ pdg/custom-module-pass.cpp
@@ -1,25 +1,29 @@
 #include "llvm/Pass.h"
 #include "llvm/IR/Module.h"
 #include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/CFG.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/Support/raw_ostream.h"
 
 using namespace llvm;
 namespace {
-  struct InstructionCounter : public ModulePass {
+  struct CustomModulePass : public ModulePass {
     static char ID;
-    InstructionCounter() : ModulePass(ID) {}
+    CustomModulePass() : ModulePass(ID) {}
 
     bool runOnModule(Module &M) override {
       int bb_id = 0;
       for (auto& F : M) {
         for (auto& BB : F) {
           errs() << "Basic block"<< bb_id++ << "\n" << BB << "\n";
+          for(auto* pred: predecessors(&BB)) {
+          errs() << "predecessor: "<< *pred << "\n";
+          }
         }
         }
       return false;
       }
     };
 }
-char InstructionCounter::ID = 0;
-static RegisterPass<InstructionCounter> X("instruction-counter", "InstructionCounter Pass");
+char CustomModulePass::ID = 0;
+static RegisterPass<CustomModulePass> X("custom-module-pass", "CustomModulePass Pass");
