853bf70fb6597051f3b0eec5111ede44cfc922ae
anant-357 <anantsh.88@gmail.com>
2024-03-26 17:32:30 +0530

    user-input

--- tests/sample.cpp
+++ tests/sample.cpp
@@ -2,23 +2,24 @@
 
 using namespace std;
 
-int fun1(int x){
+int fun1(int x) {
   cout << "printing arg x in fun1 : " << x << "\n";
   return 2;
 }
 
-int fun2(int x){
+int fun2(int x) {
   cout << "printing arg x in fun2 : " << x << "\n";
   return 3;
 }
 
-
-int main(){
-  int c = 1;
+int main() {
+  int c, d;
+  cin >> d;
+  cin >> c;
   if (c == 2) {
-    fun1(c);
+    fun1(d);
   } else {
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < c; i++) {
       fun2(i);
     }
   }
